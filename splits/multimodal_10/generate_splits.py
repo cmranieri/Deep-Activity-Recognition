af622319bac82b77a96ea738bc1e015db99d09c3
Caetano Mazzoni Ranieri <cmranieri2@gmail.com>
2019-02-28 13:51:04 -0300

    10-fold

--- splits/multimodal_10/generate_splits.py
+++ splits/multimodal_10/generate_splits.py
@@ -8,10 +8,10 @@ for fold in range(10):
         act_str = '{:02d}'.format(act+1)
         for seq in range( 9 ):
             tr_seq_str = '{:02d}'.format( (seq + fold) % 10 + 1 )
-            trainfile.write( 'act'+act_str + '/act'+act_str + 'seq'+tr_seq_str + '\n' )
+            trainfile.write( 'act'+act_str + '/act'+act_str + 'seq'+tr_seq_str + '.mp4\n' )
 
         if fold == 0:
             te_seq_str = '{:02d}'.format( 10 )
         else:
             te_seq_str = '{:02d}'.format( fold % 10 )
-        testfile.write( 'act'+act_str + '/act'+act_str + 'seq'+te_seq_str + '\n' )
+        testfile.write( 'act'+act_str + '/act'+act_str + 'seq'+te_seq_str + '.mp4\n' )
--- splits/multimodal_10/txt_to_npy.py
+++ splits/multimodal_10/txt_to_npy.py
@@ -2,11 +2,17 @@ import numpy as np
 
 for fold in range(10):
     fold_id = '{:02d}'.format(fold+1)
-    sfile = open( 'trainlist'+fold_id+'.txt' , 'r' )
-    sfile = open( 'testlist' +fold_id+'.txt' , 'r' )
 
+    sfile = open( 'trainlist'+fold_id+'.txt' , 'r' )
     dlist = list()
     for line in sfile:
         dlist.append( line.strip('\n') )
     np.save( 'trainlist'+fold_id+'.npy' , np.array(dlist) )
+    sfile.close()
+
+    sfile = open( 'testlist' +fold_id+'.txt' , 'r' )
+    dlist = list()
+    for line in sfile:
+        dlist.append( line.strip('\n') )
     np.save( 'testlist' +fold_id+'.npy' , np.array(dlist) )
+    sfile.close()
