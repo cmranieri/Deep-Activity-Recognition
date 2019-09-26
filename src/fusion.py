a33a4f5fad7b161f8ff88d0998a9dccd050e9a13
Caetano Ranieri <cmranieri2@gmail.com>
2019-09-26 07:49:14 -0300

    multimodal tcn

--- src/fusion.py
+++ src/fusion.py
@@ -2,24 +2,24 @@ import numpy as np
 import pickle
 
 
-a = 0
+a = 1
 b = 1
 c = 0
 
 acc_list = list()
-for j in range(1):
+for j in range(8):
     split_f = '{:02d}'.format( j+1 )
     #out_a = pickle.load( open ('../outputs/model-ucf101-spatial-mobilenet.pickle', 'rb') )
     #out_b = pickle.load( open ('../outputs/model-ucf101-stack-inres.pickle', 'rb') )
-    out_a = pickle.load( open ('../outputs/model-multi-spatial-m'+str(j+1)+'.pickle', 'rb') )
-    out_b = pickle.load( open ('../outputs/model-ucf101-stack-inception_tl_multi-l'+split_f+'.pickle', 'rb') )
+    out_a = pickle.load( open ('../outputs/model-utd-vlstm-'+split_f+'.pickle', 'rb') )
+    #out_b = pickle.load( open ('../outputs/model-ucf101-stack-inception_tl_multi-l'+split_f+'.pickle', 'rb') )
     #out_c = pickle.load( open ('../outputs/caetano-cnn-lstm-l'+str(j+1)+'.pickle', 'rb') )
     correct_list = list()
 
-    for i in range( len( out_b['labels'] ) ):
-        pred = a * out_a['predictions'][i] + b * out_b['predictions'][i] #+ c * out_c['predictions'][i]
+    for i in range( len( out_a['labels'] ) ):
+        pred = a * out_a['predictions'][i]# + b * out_b['predictions'][i] #+ c * out_c['predictions'][i]
         #pred = out_b['predictions'][i] #+ c * out_c['predictions'][i]
-        label = out_b['labels'][i]
+        label = out_a['labels'][i]
         correct = np.equal( np.argmax( pred ),
                             np.argmax( label ) )
         correct_list.append(correct)
@@ -27,3 +27,4 @@ for j in range(1):
     acc_list.append( np.mean( correct_list ) )
 
 print(np.mean(acc_list))
+print(np.std(acc_list))
--- src/train_model.py
+++ src/train_model.py
@@ -1,4 +1,4 @@
-from Multimodal_LSTM import Multimodal_LSTM as Network
+from Multimodal_TCN import Multimodal_TCN as Network
 import os
 import sys
 
@@ -10,25 +10,28 @@ split_f = '{:02d}'.format( split )
 network = Network( flowDataDir  = '/home/cmranieri/datasets/UTD-MHAD/flow',
                    imuDataDir   = '/home/cmranieri/datasets/UTD-MHAD/Inertial_csv',
                    modelDir     = '/home/cmranieri/models/utd-mhad',
-                   modelName    = 'model-utd-clstm-%s' % split_f,
+                   modelName    = 'model-utd-ctcn-%s' % split_f,
                    cnnModelName = 'model-ucf101-optflow-inception',
                    trainListPath = '../splits/utd-mhad/trainlist%s.txt' % split_f,
                    testListPath  = '../splits/utd-mhad/testlist%s.txt' % split_f,
                    lblFilename  = '../classes/classIndUtd.txt',
-                   imuShape     = ( 30, 6 ),
+                   imuShape     = ( 100, 6 ),
                    classes      = 27,
+                   useFlips     = False,
                    flowSteps    = 15,
-                   imuSteps     = 30,
+                   imuSteps     = 100,
+                   adjust       = True,
                    framePeriod  = 2,
                    clipTh       = 20,
                    restoreModel = False,
                    normalize    = False )
 
-network.train( steps        = 20000,
+network.train( steps        = 30000,
                stepsToEval  = 30000, 
                batchSize    = 16, 
-               numThreads   = 4,
-               maxsize      = 12 )
+               numThreads   = 3,
+               maxsize      = 12,
+               evalPer      = False )
 
 network.evaluate( numSegments  = 2,
                   storeTests   = True )
